1382110f6d1a036bdb59b3214497e3a748024dac
pdineshyadav <p.dineshyadav@yahoo.in>
2016-10-07 14:02:05 -0400

    Add project files.

---
+++ Comp229Project1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Comp229Project1.Global" Language="C#" %>
---
+++ Comp229Project1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp229Project1.Startup))]
+namespace Comp229Project1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
