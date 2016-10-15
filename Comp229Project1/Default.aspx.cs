720e060d2485ca3dcaa03cee71d55964f268a3ea
pdineshyadav <p.dineshyadav@yahoo.in>
2016-10-14 22:53:16 -0400

    Connected to the database

--- Comp229Project1/Default.aspx.cs
+++ Comp229Project1/Default.aspx.cs
@@ -9,12 +9,10 @@ using System.Web.ModelBinding;
 
 namespace Comp229Project1
 {
-    public partial class _Default : Page
+    public partial class Students : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-
             // if loading the page for the first time
             // populate the student grid
             if (!IsPostBack)
@@ -22,21 +20,55 @@ namespace Comp229Project1
                 // Get the student data
                 this.GetStudents();
             }
-
         }
+
+        /// <summary>
+        /// This method gets the student data from the DB
+        /// </summary>
         private void GetStudents()
         {
             // connect to EF DB
             using (Model01 db = new Model01())
             {
                 // query the Student Table using EF and LINQ
-                var Students = (from allTable in db.Tables
-                                select allTable);
+                var Students = (from allStudents in db.Tables
+                                select allStudents);
 
                 // bind the result to the Students GridView
                 StudentsGridView.DataSource = Students.ToList();
                 StudentsGridView.DataBind();
             }
+        }
+
+        protected void StudentsGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+
+            // store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            // get the selected StudentID using the Grid's DataKey collection
+            int StudentID = Convert.ToInt32(StudentsGridView.DataKeys[selectedRow].Values["StudentID"]);
+
+            // use EF and LINQ to find the selected student in the DB and remove it
+            using (ContosoContext db = new ContosoContext())
+            {
+                // create object ot the student clas and store the query inside of it
+                Student deletedStudent = (from studentRecords in db.Students
+                                          where studentRecords.StudentID == StudentID
+                                          select studentRecords).FirstOrDefault();
+
+                // remove the selected student from the db
+                db.Students.Remove(deletedStudent);
+
+                // save my changes back to the db
+                db.SaveChanges();
+
+                // refresh the grid
+                this.GetStudents();
+            }
+
+
         }
     }
 }
